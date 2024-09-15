a71f27e096d7febb226085ea58012b6fa894941b
jyothika28 <jyothikajalla032@gmail.com>
2024-09-15 18:50:44 -0400

    resolves #2

--- bank-demo/src/bank.ts
+++ bank-demo/src/bank.ts
@@ -9,7 +9,7 @@ export class Bank{
     createAccount(name: string, age: number): Account | string{
         const existingAccount = accounts.find(acc => acc.name === name && acc.age === age);
         if(existingAccount){
-            return 'Account already exists. Failed to create ${existingAccount.name} account';
+            return `Account already exists. Failed to create ${existingAccount.name} account`;
         }
         const newAccount: Account = {
             id: (accounts.length + 1).toString(),
@@ -21,5 +21,12 @@ export class Bank{
     return newAccount;
 }
     // User Story 2: Deposit money into account
-   
+   deposit(accountId: string, amount: number): string {
+    const account = accounts.find(acc => acc.id === accountId);
+    if(!account){
+        return 'Account not found';
+    }
+    account.balance += amount;
+    return `Deposited ${amount} into ${account.name} account. New balance is ${account.balance}`;
+   }
 }
--- bank-demo/src/bankTest.ts
+++ bank-demo/src/bankTest.ts
@@ -6,7 +6,18 @@ const bank = new Bank();
 
 // Test case: create account
 //This test case should create a new account with name Bhavani and age 40
+console.log("**** User Story 1: Create account ****");
 console.log(bank.createAccount("Bhavani",40));
 //This test case should return 'Account already exists' as the account with name Bhavani and age 40 already exists
 console.log(bank.createAccount("Bhavani",40));
 console.log(bank.createAccount("Shiva",25));
+
+
+//User Story 2: Deposit money into account
+// Test case: deposit money into account
+//This test case should deposit 1000 into Rory account
+console.log("**** User Story 2: Deposit money into account ****");
+const roryAccount=bank.createAccount("Rory",30) as {id:string};
+console.log(bank.deposit(roryAccount.id,1000));
+//This test case should return 'Account not found' as the account with id 10 does not exist
+console.log(bank.deposit("10",1000));
